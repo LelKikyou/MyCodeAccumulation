acc8f00daec02eb1cab9d2e4ba9d0280be4c1dba
黄琪勇 <7344498+huang_qi_yong@user.noreply.gitee.com>
2021-04-19 16:44:27 +0800

    自动化

--- 12.前端自动化/webhooks/webhook.js
+++ 12.前端自动化/webhooks/webhook.js
@@ -1,17 +1,28 @@
 var http = require("http")
+var shell = require("shelljs")
 var createHandler = require("gitee-webhook-handler")
 var handler = createHandler({ path: "/webhooks_push", secret: "123456" }) //# post 所需要用到的秘钥
 
-function run_cmd(cmd, args, callback) {
-  var spawn = require("child_process").spawn
-  var child = spawn(cmd, args)
-  var resp = ""
-  child.stdout.on("data", function (buffer) {
-    resp += buffer.toString()
-  })
-  child.stdout.on("end", function () {
-    callback(resp)
-  })
+function run_cmd() {
+  shell.echo("cd start")
+  shell.cd("./video_booking_system")
+  shell.echo("cd end")
+
+  shell.echo("git pull start")
+  shell.exec("git pull")
+  shell.echo("git pull end")
+
+  shell.echo("npm install start")
+  shell.exec("npm install")
+  shell.echo("npm install end")
+
+  shell.echo("npm run build start")
+  shell.exec("npm run build")
+  shell.echo("npm run build end")
+
+  shell.echo("dist -r start")
+  shell.cp("-R", "./dist/*", "../../nginx/html/guangxiyuyue")
+  shell.echo("all end")
 }
 handler.on("error", function (err) {
   console.error("Error:", err.message)
@@ -19,9 +30,7 @@ handler.on("error", function (err) {
 handler.on("Push Hook", function (event) {
   //# 这个地方就是GitHub 和 Gitee 不一样的地方，需要注意
   console.log("Received a push event for %s to %s", event.payload.repository.name, event.payload.ref)
-  run_cmd("sh", ["./deploy.sh"], function (text) {
-    console.log(text)
-  }) //# 需要执行的脚本位置
+  run_cmd() //# 需要执行的脚本位置
 })
 try {
   http
